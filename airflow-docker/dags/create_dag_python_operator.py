b3309484f784c0ecd9cb6aecff7e55f3ec826191
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-07 16:09:38 +0300

    have learned how to connect two tasks together in airflow using xcoms

--- airflow-docker/dags/create_dag_python_operator.py
+++ airflow-docker/dags/create_dag_python_operator.py
@@ -12,19 +12,28 @@ def greet():
     print("hello there this is my first python operator dag")
 
 
-def greet_with_age(name,age):
+def greet_with_age(age,ti):
+    name = ti.xcom_pull(task_ids='get_nameo')
     print(f"Hello, my name is {name}. And I am {age} years old!..")
 
+
+def get_name():
+    return 'marto'
+
 with DAG(
-    dag_id='our_second_python_operator_dag',
+    dag_id='our_fourth_python_operator_dag',
     default_args=default_args,
     description='our first dag using python operator',
     start_date=datetime(2022,7,6,2),
     schedule_interval='@daily'
 )as dag:
     task1 = PythonOperator(
-        task_id='greeter',
+        task_id='greet',
         python_callable=greet_with_age,
-        op_kwargs={'name':'Martin','age':25}
+        op_kwargs={'age':24}
     )
-    task1
+    task2 = PythonOperator(
+        task_id='get_nameo',
+        python_callable=get_name,
+    )
+    task2 >> task1
