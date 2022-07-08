a806206ab46a2f55edf66f37f2013239da2f1fe9
nardoshood <nardostilahun83@gmail.com>
2022-07-08 22:41:26 +0300

    header tag added

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './header.css';
+import ai from '../../assets/ai.webm'
+
+const Header = () => (
+  <div className="header section__padding" id="home">
+    <div className="header-content">
+      <h1 className="gradient__text">የአማርኛ ድምፅ መረጃ መሰብሰቢያ መሳሪያ</h1>
+      <p>ይህ የአማርኛ ቋንቋ የመረጃ መሰብሰቢያ መሳሪያ ነው። ለቀረበው ጽሑፍ የድምጽ መረጃን ለመሰብሰብ ፈቃደኛ ስለሆኑ እናመሰግናለን። ለመጀመር እባክዎ ቀጣዩን ቁልፍ ይጫኑ</p>
+
+      <div className="header-content__input">
+        <button type="button">ጀምር</button>
+      </div>
+
+   </div>
+
+    <div className="header-image">
+      <video src={ai}loop muted autoPlay/>
+    </div>
+  </div>
+);
+
+export default Header;
