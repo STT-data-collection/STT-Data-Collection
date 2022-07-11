b8e97abc9c82ddd663dc8b6586fb8c5ee13d3536
nardoshood <nardostilahun83@gmail.com>
2022-07-11 22:13:53 +0300

    app connection to kafka

--- API/app.py
+++ API/app.py
@@ -9,51 +9,55 @@ import numpy as np
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.after_request
-def after_request(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add(
-        "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-    response.headers.add("Access-Control-Allow-Credentials", "true")
-    return response
+TEXT_TOPIC = "spark-transformed-text"
+TEXT_AUDIO_PAIR_TOPIC = "text.audio.pair"
+BROKER_ADDRESS = 'localhost:39092'
 
+try:
+        producer = KafkaProducer(bootstrap_servers=BROKER_ADDRESS,
+                                 value_serializer=lambda x: dumps(x).encode('utf-8'))
+        consumer = KafkaConsumer(TEXT_TOPIC,
+                                 bootstrap_servers=BROKER_ADDRESS,
+                                 auto_offset_reset='earliest',
+                                 enable_auto_commit=False,
+                                 value_deserializer=lambda x: loads(x.decode('utf-8')))
+except NoBrokersAvailable:
+        print("NoBrokersAvailable")
 
-def convert(o):
-    if isinstance(o, np.generic):
-        return o.item()
-    raise TypeError
-
-
-
-@app.route("/", methods=["GET"])
-def home():
-    return json.dumps({"result": "This is home"}, default=convert)
-
-@app.route("/get-text", methods=["GET"])
-def text_getter():
-    return json.dumps({"result": "This is response text"}, default=convert)
-
-@app.route("/send-audio", methods=["POST", "GET"])
-def file_getter():
-    if request.method == "POST":
+@app.route('/get-text', methods=["GET"])
+def get_text():
+        print("sending text")
         try:
-            file = request.files["file"]
-            print(file)
-           
-            return json.dumps(
-                {"success": "ለቋንቋው እድገት ድጋፍ ስላደረጉት እናመሰግናለን::"}, default=convert
-            )
-            
-        except Exception as e:
-            print(e)
-            return json.dumps(
-                {"error": "Couldn't handle uploaded file. "}, default=convert
-            )
-    else:
-        print("error")
-        return json.dumps({"error": "No POST Data"}, default=convert)
+            for s in consumer:
+                print(s.value)
+                article = s.value
+                return jsonify(text=article)
+        except NameError:
+            print("Consumer not init")
+            return 404
+
+@app.route('/send-audio', methods=["POST"])
+def publish_text_audio_pair():
+        audio = request.files['audio']
+        article = audio.filename
+        audio = extract_audio(audio)
+        print(audio.shape)
+        audio = audio.tolist()
+        id = get_uuid()
+        data = {
+            "id": id,
+            "article": article,
+            "audio": audio
+        }
+        try:
+            res = producer.send(TEXT_AUDIO_PAIR_TOPIC, value=data)
+            print(res)
+        except NameError:
+            print("Producer not created")
+
+        # pprint(data)
+
+        return "200"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
